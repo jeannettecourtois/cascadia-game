81434b380d8a19031478695dfb3b939b83737ba6
Corentin Kerampran <corentin.kerampran@etu.utc.fr>
2025-05-10 06:36:18 +0000

    Add new file

---
+++ src/Pioche.h
@@ -0,0 +1,46 @@
+#pramga once
+#include "Tuile.h"
+#include "ControleurGeneral.h"
+
+
+class Pioche {
+private:
+    Tuile* tuiles[4];
+    animalOuPas jetons[4];
+    int idTuileChoisis;
+    SacTuile* sacDeTuiles;
+    SacJeton* sacDeJetons;
+public:
+    Pioche();
+    ~Pioche();
+    void PreparerPioche();
+    void SelectionnerTuile();
+    void SelectionnerJeton();
+}
+
+class SacTuile {
+private:
+    int nbTuile;
+    Tuile** tuiles;
+public:
+    SacTuile();
+    ~SacTuile();
+    Tuile* Piocher();
+    void MettreDansLeSac(Tuile&);
+}
+
+
+class SacJeton {
+private:
+//Est-ce qu'il serait pas mieux de faire une liste basé sur l'énumération animal => on peut rajouter des animaux dans la liste
+    int nbOurs;
+    int nbCerf;
+    int nbSaumon;
+    int nbAigle;
+    int nbRenard;
+public:
+    SacJeton();
+    ~SacJeton();
+    animal* Piocher();
+    void MettreDansLeSac(animal);
+}
