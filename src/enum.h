c6ce1c855ad849a7b141d9fdb499210c1800dbef
Corentin Kerampran <corentin.kerampran@etu.utc.fr>
2025-05-10 06:09:43 +0000

    Add new file

---
+++ src/enum.h
@@ -0,0 +1,26 @@
+#pragma once
+
+enum animal {
+    Ours = 1,
+    Cerf,
+    Saumon,
+    Aigle,
+    Renard
+};
+
+enum animalOuPas {
+    Vide = 0,
+    Ours = 1,
+    Cerf, 
+    Saumon,
+    Aigle,
+    Renard
+};
+
+enum habitat {
+    Montagne,
+    Foret,
+    Prairie,
+    Marais,
+    Fleuve
+};
