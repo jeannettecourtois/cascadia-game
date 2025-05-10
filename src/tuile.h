8144e2903b0766fa4d3f84a78bf6e7545e010313
Corentin Kerampran <corentin.kerampran@etu.utc.fr>
2025-05-10 06:23:09 +0000

    Add new file

---
+++ src/tuile.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "enum.h"
+
+
+class Tuile {
+private:
+    const int nbAnimaux;
+    const int nbHabitat;
+    animal** listeAnimaux;
+    habitat** listeHabitat;
+public:
+    Tuile();
+    ~Tuile();
+    void AssociationJeton() const {}
+    int GetNbAnimaux() const {return nbAnimaux}
+    int GetNbHabitat() const {return nbHabitat}
+    animal** GetListeAnimaux() const {return listeAnimaux}
+    habitat** GetListeHabitat() const {return listeHabitat}
+}
+
+class Keystone : public Tuile {
+public:
+    void AssociationJeton() const; //Est-ce qu'on met joueur en paramètre ?
+}
