9cdc5e39b6c9fb54bcd48905d348a0d07760e9fc
Jeannette Garea Courtois <jeannette.garea.c@gmail.com>
2025-05-24 09:31:11 +0200

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,28 @@
 #include "carteMarquageFaune.h"
 
 int main() {
-    std::cout << "Projet Cascadia prêt !" << std::endl;
+
+    unsigned int monChoix;
+    cout<<"Bienvenur à notre jeu Cascadia!\n";
+    cout<<"Voici les choix: \n";
+    cout<<"Choix 0: Démarrer le jeu.\n";
+    cout<<"Choix 1: Arrêter le jeu.\n";
+    cout<<"Quel est votre choix ?\n";
+    cout<<"Voitre choix: \n";
+    cin>>monChoix;
+    switch(monChoix){
+        case 0: 
+        cout<<"Le jeu va démarrer!\n";
+        int nbJoueurs;
+        cout<<"Vous êtes combien de joueurs?\n";
+        cin>>nbJoueurs;
+        break;
+        case 1: 
+        cout<<"Arrêt du jeu\n";
+        cout<<"Voici votre score: \n";
+        break;
+        default: 
+        cout<<"Arrêt du jeu\n";
+    }
     return 0;
 }
