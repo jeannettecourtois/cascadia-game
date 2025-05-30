2719b7b0b972913131ae169b23bf137b26bd253a
yan-s <110732997+Toppics@users.noreply.github.com>
2025-05-30 09:20:39 +0200

    correction

--- src/pioche.cpp
+++ src/pioche.cpp
@@ -1,6 +1,7 @@
 #include "pioche.h"
 #include "enum.h"
 
+/* SacTuile */
 SacTuile::SacTuile() {}
 
 SacTuile::~SacTuile() {}
@@ -9,17 +10,16 @@ Tuile* SacTuile::Piocher() {return nullptr;} //à changer, juste pour compiler c
 
 void SacTuile::mettreDansLeSac(Tuile&) {}
 
-
+/* SacJeton */
 SacJeton::SacJeton() {}
 
 SacJeton::~SacJeton() {}
 
 Animal* SacJeton::Piocher() {return nullptr;} //à changer, juste pour compiler coder
 
-void SacJeton::mettreDansLeSac(Animal) {}
-
 void SacJeton::mettreDansLeSac(const Animal& a) {}
 
+/* Pioche */
 Pioche::Pioche() {
     for (int i = 0; i < 4; ++i) {
         tuiles[i] = nullptr;
